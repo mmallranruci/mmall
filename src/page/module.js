54a38e2dd13696cb89b2072287c1850e68507a90
ranruci <909266212@qq.com>
2017-12-23 22:39:56 +0800

    inital

---
+++ src/page/module.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: ranruci 
+ * @Date: 2017-12-23 17:32:37 
+ * @Last Modified by: ranruci
+ * @Last Modified time: 2017-12-23 17:33:08
+ */
+
+ 'use strict'
+ module.exports = {
+     test:1234
+ }
